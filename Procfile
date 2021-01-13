873f71f724d2fea1748fcb03f0a587e73cb47e5b
Alikhalid2020 <lanrakhaled@gmail.com>
2021-01-13 12:14:33 +0300

    add

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn khalid.wsgi --log-file -
---
+++ gallery/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+    # url('^$',views.today_imae,name = 'welcome'),
+    url(r'$',views.gallery_today,name='galleryToday'),
+    url(r'search/', views.search, name='search'),
+]
+
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
